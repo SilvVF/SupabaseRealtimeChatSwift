2df5da00ff965e8fa688c2c04f69036d79419c63
David Silver <david.silver232@gmail.com>
2022-11-26 15:11:46 -0500

    Initial Commit

---
+++ SupaBaseSwiftChat/SupaBaseSwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaBaseSwiftChatApp.swift
+//  SupaBaseSwiftChat
+//
+//  Created by David Silver on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SupaBaseSwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SupaBaseSwiftChatUITests/SupaBaseSwiftChatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SupaBaseSwiftChatUITestsLaunchTests.swift
+//  SupaBaseSwiftChatUITests
+//
+//  Created by David Silver on 11/26/22.
+//
+
+import XCTest
+
+final class SupaBaseSwiftChatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
